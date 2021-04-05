972da8b8a5917b462c60a8490a36be6f94ba09b6
Arthur CS <arthur.cs147@gmail.com>
2021-04-04 22:19:36 -0300

    Created to toggle the sub-menu when click in the image.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
 const hamburger = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
+let done = false
 
 hamburger.onclick = () => {
     setTimeout(() => {
         hamburger.classList.toggle('open')
         menu.classList.toggle('visible')
     }, 100)
+}
+
+const img = document.querySelector('.last-arrow');
+const subMenu = document.querySelector('.sub-menu');
+
+img.onclick = () => {
+    if(!done === true) {
+        setTimeout(() => {
+            img.style.transform = 'rotate(180deg)';
+            subMenu.style.display = 'block';
+            subMenu.style.visibility = 'visible';
+        }, 100)
+        done = true;
+    }else{
+        setTimeout(() => {
+            img.style.transform = 'rotate(-180deg)';
+            subMenu.style.display = 'none';
+            subMenu.style.visibility = 'hidden';
+        }, 100)
+
+        done = false;
+    }
 }
