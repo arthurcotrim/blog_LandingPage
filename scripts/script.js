312ff3ee074f44651242f97ab1f55edc0f814ca7
Arthur CS <arthur.cs147@gmail.com>
2021-04-26 15:06:38 -0300

    Created a const header to select the header tag and a welcome to select the section.welcome to toggle the visibility between the classes visible and invisible.

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,38 @@
 const hamburger = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
+const welcome = document.querySelector('.welcome');
+const img = document.querySelector('.last-arrow');
+const subMenu = document.querySelector('.sub-menu');
+const header = document.querySelector('header');
+
 let done = false
 
 hamburger.onclick = () => {
     setTimeout(() => {
-        hamburger.classList.toggle('open')
-        menu.classList.toggle('visible')
+        hamburger.classList.toggle('open');
+        menu.classList.toggle('visible');
+        welcome.classList.toggle('invisible');
+        img.style.transform = 'rotate(0deg)';
+        subMenu.style.display = 'none';
+        subMenu.style.visibility = 'hidden';
     }, 100)
 }
 
-const img = document.querySelector('.last-arrow');
-const subMenu = document.querySelector('.sub-menu');
-
 img.onclick = () => {
-    if(!done === true) {
+    if (!done === true) {
         setTimeout(() => {
             img.style.transform = 'rotate(180deg)';
             subMenu.style.display = 'block';
             subMenu.style.visibility = 'visible';
+            header.style.marginBottom = '15rem';
         }, 100)
         done = true;
-    }else{
+    } else {
         setTimeout(() => {
-            img.style.transform = 'rotate(-180deg)';
+            img.style.transform = 'rotate(0deg)';
             subMenu.style.display = 'none';
             subMenu.style.visibility = 'hidden';
+            header.style.marginBottom = '5rem';
         }, 100)
 
         done = false;
